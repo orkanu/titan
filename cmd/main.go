00fbf49cfe369c769b5e4d7678dc505a8de09a76
Marc <maruku@gmail.com>
2025-06-12 20:22:04 +0100

    refactor: move code around to be better organised

--- cmd/main.go
+++ cmd/main.go
@@ -3,10 +3,10 @@ package main
 import (
 	"fmt"
 	"os"
-	"os/exec"
 	"strings"
 	"sync"
 	"titan/internal/actions"
+	"titan/internal/config"
 	"titan/internal/utils"
 )
 
@@ -16,7 +16,7 @@ func main() {
 		utils.PrintlnRed(fmt.Sprintf("Error parsing flags: %v", err))
 		os.Exit(1)
 	}
-	cfg, err := NewConfig(flagsData.ConfigPath)
+	cfg, err := config.NewConfig(flagsData.ConfigPath)
 	if err != nil {
 		utils.PrintlnRed(fmt.Sprintf("Error retrieving configuration: %v", err))
 		os.Exit(1)
@@ -39,7 +39,7 @@ func main() {
 	}
 
 	// Setup nvm and pnpm environment
-	env, err := captureEnvironment(cfg.Versions)
+	env, err := utils.CaptureEnvironment(cfg.Versions)
 	if err != nil {
 		utils.PrintlnRed(fmt.Sprintf("Error setting up shared bash environment: %v", err))
 		os.Exit(1)
@@ -83,24 +83,6 @@ func main() {
 	} else {
 		fmt.Println("All actions completed successfully")
 	}
-
-	// Get subcomand from flags:
-	// 	(fetch) should fetch and pull from remote for each project
-	// 	(clean) should remove node_modules, dist and yalc folders on each of the different projects
-	// 	(install) should install deps for each of the different projects
-	// 	(build) should build each of the different projects
-	// 	(all) should fetch, clean, install and build each of the different projects
-	// If no subcomand specified, assume it is (all)
-	//
-	// For install & build we need to do the following:
-	// 	Check/install node (use version from config)
-	// 	Check/install pnpm (use version from config)
-	//
-	// Should use goroutines to run everything in parallel
-	// Should use prefix when logging to console, for instance:
-	// 	(FETCH): blah blah blah
-	// 	(INSTALL): blah blah blah more
-	// Evaluate using files for logging. Each action should use its own file
 }
 
 func repoFullPath(base string, repo string) string {
@@ -111,32 +93,4 @@ func repoFullPath(base string, repo string) string {
 	}
 	b.WriteString(repo)
 	return b.String()
-}
-
-// captureEnvironment sets up NVM and pnpm and captures the resulting environment
-func captureEnvironment(versions Versions) ([]string, error) {
-	// Source nvm and install the desired nvm and pnpm versions
-	scriptContents := fmt.Sprintf(`
-		source ~/.nvm/nvm.sh &&
-		nvm install %v &&
-		nvm use %v &&
-		npm i -g pnpm@%v &&
-		env
-		`, versions.Node, versions.Node, versions.PNPM)
-	setupCmd := exec.Command("bash", "-c", scriptContents)
-	output, err := setupCmd.Output()
-	if err != nil {
-		return nil, fmt.Errorf("failed to setup environment: %w", err)
-	}
-	// Parese the environment output
-	envLines := strings.Split(string(output), "\n")
-	var env []string
-	for _, line := range envLines {
-		line = strings.TrimSpace(line)
-		if line != "" && strings.Contains(line, "=") {
-			env = append(env, line)
-		}
-	}
-
-	return env, nil
 }
--- internal/utils/scripts.go
+++ internal/utils/scripts.go
@@ -1,9 +1,40 @@
 package utils
 
 import (
+	"fmt"
 	"os"
 	"os/exec"
+	"strings"
+	"titan/internal/config"
 )
+
+// captureEnvironment sets up NVM and pnpm and captures the resulting environment
+func CaptureEnvironment(versions config.Versions) ([]string, error) {
+	// Source nvm and install the desired nvm and pnpm versions
+	scriptContents := fmt.Sprintf(`
+		source ~/.nvm/nvm.sh &&
+		nvm install %v &&
+		nvm use %v &&
+		npm i -g pnpm@%v &&
+		env
+		`, versions.Node, versions.Node, versions.PNPM)
+	setupCmd := exec.Command("bash", "-c", scriptContents)
+	output, err := setupCmd.Output()
+	if err != nil {
+		return nil, fmt.Errorf("failed to setup environment: %w", err)
+	}
+	// Parese the environment output
+	envLines := strings.Split(string(output), "\n")
+	var env []string
+	for _, line := range envLines {
+		line = strings.TrimSpace(line)
+		if line != "" && strings.Contains(line, "=") {
+			env = append(env, line)
+		}
+	}
+
+	return env, nil
+}
 
 // ExecScript is a utility function that creates a shell script and executes it
 func ExecScript(script string, env []string) error {
